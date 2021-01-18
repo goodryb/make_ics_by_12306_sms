459fe1809aa290231a0943a05e0414044672de94
墨华 <renyabo2005@gmail.com>
2021-01-18 19:42:11 +0800

    use python3

--- make_12306_event_ics.py
+++ make_12306_event_ics.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import os
 import sys
@@ -17,10 +17,7 @@ default_station = [u"杭州东", u"上海虹桥", u"杭州"]
 
 def get_travel(t):
     dcc = []
-    # print t
-    m_c = re.compile(ur"\w\d*次").search(t)  # 查找车次信息
-
-    # print str(m_c.group(0)[:-1])
+    m_c = re.compile("\w\d*次").search(t)  # 查找车次信息
 
     # 获取车次网页信息
     url = "http://search.huochepiao.com/checi/" + str(m_c.group(0)[:-1])
@@ -44,15 +41,15 @@ def get_travel(t):
 
     start_station_index = 0  # 出发站序号
     start_station_name = ""  # 出发站名称
-    # print dcc
+
     for i in dcc:
         m = re.compile(i["name"]).search(t)
         if m:
             start_station_name = m.group(0)
             start_station_index = dcc.index(i)
 
-    print u"出发站：" + start_station_name, u"出发站序号：" + str(start_station_index)
-    print u"请选择目的地："
+    print(u"出发站：" + start_station_name, u"出发站序号：" + str(start_station_index))
+    print(u"请选择目的地：")
 
     k = 0  # 初始化显示站点序列号
 
@@ -62,7 +59,7 @@ def get_travel(t):
 
     for i in dcc:
         if k > start_station_index:
-            print str(k) + u"：" + i["name"]
+            print(str(k) + u"：" + i["name"])
             if i["name"] in default_station:
                 default_to_station_index = k
                 default_to_station_name = i["name"]
@@ -71,25 +68,25 @@ def get_travel(t):
     # 到达站序号
     # 自动设置到达站
     if default_to_station_index > 0:
-        print u"回车确认到达 " + default_to_station_name + u"站"
-        to_station_index = raw_input()
+        print(u"回车确认到达 " + default_to_station_name + u"站")
+        to_station_index = input()
         if not to_station_index:
             to_station_index = default_to_station_index
             return start_station_index, to_station_index, dcc
         else:
             return start_station_index, int(to_station_index), dcc
 
-    to_station_index = int(raw_input())
+    to_station_index = int(input())
     while to_station_index > len(dcc) - 1 or to_station_index <= start_station_index:
-        print u"目的地选择有误，请重新选择"
-        to_station_index = int(raw_input())
+        print(u"目的地选择有误，请重新选择")
+        to_station_index = int(input())
 
     return start_station_index, to_station_index, dcc
 
 
 def con_t(cost):
-    r_hour = re.compile(ur"\d*小时").search(cost)
-    r_min = re.compile(ur"\d*分").search(cost)
+    r_hour = re.compile("\d*小时").search(cost)
+    r_min = re.compile("\d*分").search(cost)
 
     total_min = 0
 
@@ -117,10 +114,10 @@ def get_info(t):
     start_station_name = dcc[s]["name"]
     to_station_name = dcc[e]["name"]
     cost_hour, cost_mins = get_costs(s, e, dcc)
-    cc = re.compile(ur"\w\d*次").search(t).group(0)[:-1]
-    zc = re.compile(ur"\d*车\d*\w").search(t).group(0)
-    day = re.compile(ur"\d*日").search(t).group(0)[:-1]
-    month = re.compile(ur"\d*月").search(t).group(0)[:-1]
+    cc = re.compile("\w\d*次").search(t).group(0)[:-1]
+    zc = re.compile("\d*车\d*\w").search(t).group(0)
+    day = re.compile("\d*日").search(t).group(0)[:-1]
+    month = re.compile("\d*月").search(t).group(0)[:-1]
     start_time = dcc[s]["start"]
     end_time = dcc[e]["end"]
     # check_in = re.compile(ur"检票口：\d*").search(t).group(0)[3:]
@@ -187,13 +184,13 @@ def get_12306_ROWID():
 if __name__ == "__main__":
     # t = u"订单E11053xxxx,XXX您已购4月24日G7609次x车xF号南京南13:57开,检票口B11。【铁路客服】"
     if len(sys.argv) == 2:
-        t = unicode(sys.argv[1], "utf-8")
+        t = sys.argv[1]
     if len(sys.argv) == 1:
         ROWID = get_12306_ROWID()
         t = get_messages_for_recipient(ROWID)
     if len(sys.argv) > 2:
-        print u"参数有误！"
+        print(u"参数有误！")
         sys.exit(1)
-    print u"订单信息：" + t
+    print(u"订单信息：" + t)
     make_ics(t)
     os.system('open /tmp/cal.ics')
