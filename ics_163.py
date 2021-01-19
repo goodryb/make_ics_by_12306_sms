37c20f42639d520bf35ff2933243a9ce6c6d976d
墨华 <renyabo2005@gmail.com>
2021-01-19 15:26:03 +0800

    bug fix

--- ics_163.py
+++ ics_163.py
@@ -16,8 +16,15 @@ from icalendar import Calendar, Event
 
 # 全局设定
 # 经常去的站点可以设置在这里，如果目的地中包含这些站点，会自动选中
-default_station = ["杭州东", "上海虹桥", "杭州"]
+default_station = ["杭州东", "上海虹桥"]
 mail_12306 = "12306@rails.com.cn"
+# 乘车人姓名
+name = 'xxx'
+# 输入邮件地址, 口令和POP3服务器地址:
+user = 'xxxx@163.com'
+# 此处密码是授权码,用于登录第三方邮件客户端
+password = 'xxx'
+eamil_server = 'pop.163.com'
 
 
 # 字符编码转换
@@ -29,10 +36,6 @@ def decode_str(str_in):
 
 
 def decodeBody(msgPart):
-    """
-    解码内容
-    :param msgPart: 邮件某部分
-    """
     contentType = msgPart.get_content_type()
     textContent = ""
     if contentType == 'text/plain' or contentType == 'text/html':
@@ -50,8 +53,7 @@ def decodeBody(msgPart):
 
 def get_travel(t):
     dcc = []
-    m_c = re.compile("\w\d*次").search(t)  # 查找车次信息
-    # 获取车次网页信息
+    m_c = re.compile("\w\d*次").search(t)
     url = "http://search.huochepiao.com/checi/" + str(m_c.group(0)[:-1])
     header = {'User-Agent': 'Mozilla/5.0'}
     req = requests.get(url, headers=header)
@@ -101,18 +103,16 @@ def get_travel(t):
     # 到达站序号
     # 自动设置到达站
     if default_to_station_index > 0:
-        print(u"回车确认到达 " + default_to_station_name + u"站")
-        to_station_index = input()
+        to_station_index = input("回车确认到达 " + default_to_station_name + "站: ")
         if not to_station_index:
             to_station_index = default_to_station_index
             return start_station_index, to_station_index, dcc
         else:
             return start_station_index, int(to_station_index), dcc
 
-    to_station_index = int(input())
+    to_station_index = int(input('请选择目的地，输入序号：'))
     while to_station_index > len(dcc) - 1 or to_station_index <= start_station_index:
-        print("目的地选择有误，请重新选择")
-        to_station_index = int(input())
+        to_station_index = int(input("目的地选择有误，请重新选择： "))
 
     return start_station_index, to_station_index, dcc
 
@@ -136,7 +136,7 @@ def get_costs(s, e, dcc):
     s_total = con_t(dcc[s]["cost"])
     e_total = con_t(dcc[e]["cost"])
 
-    hour = (e_total - s_total) / 60
+    hour = (e_total - s_total) // 60
     mains = (e_total - s_total) % 60
 
     return hour, mains
@@ -161,6 +161,7 @@ def get_info(t):
 def make_ics(t):
     start_station_name, to_station_name, cost_hour, cost_mins, cc, zc, day, month, year, start_time, end_time = get_info(
         t)
+
     cal = Calendar()
     event = Event()
     event.add('dtstart',
@@ -186,7 +187,6 @@ def get_mgs_content(server, id):
     msg = Parser().parsestr(msg_content)
     From = parseaddr(msg.get('from'))[1]
     To = parseaddr(msg.get('To'))[1]
-    # Cc = parseaddr(msg.get_all('Cc'))[1]  # 抄送人
     Subject = decode_str(msg.get('Subject'))
 
     # 获取邮件正文内容
@@ -215,8 +215,11 @@ def run_ing(user, password, pop3_server):
     # 身份认证:
     server.user(user)
     server.pass_(password)
+
     # list()返回所有邮件的编号:
     resp, mails, octets = server.list()
+    # 可以查看返回的列表类似[b'1 82923', b'2 2184', ...]
+    # print(mails)
 
     index = len(mails)
     mail_list = []
@@ -225,23 +228,23 @@ def run_ing(user, password, pop3_server):
         From = msg_dic.get('From')
         msgBodyContents = msg_dic.get('content')
         if From == mail_12306:
-            t = msgBodyContents[0].split('\r\n')[7]
-            mail_list.append(t)
+            for o in msgBodyContents[0].split('\r\n'):
+                if name in o:
+                    mail_list.append(o)
 
     for j in mail_list:
-        # 打印车次信息
-        print("ID:", mail_list.index(j), j.split('.')[1:][0])
+        # 打印车次列表
+        print("ID:", mail_list.index(j) + 1, j.split('.')[1:][0])
 
-    print('请选择要生成日历事件的订单ID')
-    t_id = int(input())
-    make_ics(mail_list[t_id])
+    s = input('请选择要生成日历事件的订单ID，回车选择第1个订单: ')
+    if not s:
+        t_id = 1
+    else:
+        t_id = int(s)
+
+    make_ics(mail_list[t_id - 1])
     os.system('open /tmp/cal.ics')
 
 
 if __name__ == '__main__':
-    # 输入邮件地址, 口令和POP3服务器地址:
-    user = 'xxxxxxx@163.com'
-    # 此处密码是授权码,用于登录第三方邮件客户端
-    password = 'xxxxxxxxxxxxxx'
-    eamil_server = 'pop.163.com'
     run_ing(user, password, eamil_server)
